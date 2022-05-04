2f50a5543a0f3e9d14c99c4192e2fae78d31a023
wolfzart <sebastian.gonzalez8@mail.udp.cl>
2022-05-04 01:15:12 -0400

    avanzado

--- intento/Client/app.js
+++ intento/Client/app.js
@@ -2,34 +2,168 @@ const express = require('express');
 const cors = require("cors");
 const axios = require("axios");
 const https = require('https')
+const redis = require('redis');
+
+const client = redis.createClient({
+  url: 'redis://@redis-server:6379'
+});
+
+console.log("Esperando a redis para iniciar");
+
+client.on("error", function (error) {
+  console.error(error);
+});
+
+client.on('connect', () => {
+  console.log('Cliente Redis, conectado');
+});
+
+client.connect();
+
 const app = express();
 
 
 const port=3000;
 
 //FUNCION QUE HACE EL LLAMADO AL SERVIDOR
-/*
-app.get("/inventory/search",(req, res) => {
-    
-    const item = req.query.q;
-    console.log(item)
-    async () => { await axios.get('http://grcp:3001/items', { params:{q: item}}).then(res2 => {
-        //console.log(`statusCode: ${res.status}`)
-        console.log('Data:',res2.data);
-        //let data = JSON.stringify(res2.data)
-        //client.set(item, data);
-        cache = res2.data;
-        res.json(cache);
-      }).catch(error => {console.error(error)})
-    
-}
-        //await axios.get('http://grcp:3001/items', { params:{name: item}}).then(res.json)    
-});*/
 
 //-------------
+
+// Obtiene todas las keys de redis
+app.get("/keys", async (req, res) => {
+  try {
+      console.log("hola 1")
+      var all_keys = await client.keys(`*`);
+      var response = await Promise.all(all_keys.map(async key => {
+          let new_item = {}
+          let temp = await client.get(key)
+          new_item[key] = JSON.parse(temp)
+          return new_item
+      }))
+      console.log("hola 2", response)
+      res.json(response);
+      console.log("hola 3")
+  } catch (error) {
+      console.error("Error: ", error)
+      return res.status(500).json(error)
+  }
+
+});
+
+app.get("/key", async (req, res) => {
+  console.log("Searching for: " + req.query.key);
+  const key = req.query.key;
+  
+  const keys =  client.keys(`*${key}*`);
+  if (keys.length == 0) {
+      console.log("No encontrado en cache");
+      res.send("No encontrado en cache");
+  }
+  else{
+      console.log("Encontrado en cache");
+      
+      console.log(keys);
+      
+      response = await client.get(key);
+
+      console.log(response); 
+
+      res.send({ value : response });
+  
+  }
+});
+
+app.get('/inventory/search', async (req, res) => {
+  const item = req.query.q;
+
+  try {
+      let peticion = await axios.get('http://grcp:3001/items', { params: { name: item }, headers: {
+          'Content-Type': 'application/json',
+      }});
+      if(peticion.status == 200){
+          console.log("Salio bien")
+      }
+      let data = peticion.data
+      //console.log("aqui esta la peticion",data)
+      let guardado = JSON.stringify(data);
+      await client.set(item, guardado)
+      console.log(await client.get(item))
+      return res.json(data);
+  } catch (error) {
+      console.error("Error: ", error)
+      return res.status(500).json(error)
+  }
+  
+});
+/* intento 10000 pipe
 app.get('/inventory/search', (req, res) => {
+  const item = req.query.q;
+  (async () => {
+    var respuesta = await axios.get('http://grcp:3001/items', { params: {name: item}}).then( async x => {
+      console.log(x,x.status,x.data)
+      let response = await x.data
+      return response;
+    }).then(res2 => {
+
+    console.log(res2)
+    console.log("statusCode:", res2.status)
+    //console.log(res)
+    data = JSON.stringify(res2.data);
+     
+    client.set(item,data)
+    res.json(data);
+    console.log(client.get(item))
+  }).catch(error => {console.error(error)})
+
+  })();
+});
+
+*/
+//principal
+/*app.get('/inventory/search', (req, res) => {
     const item = req.query.q;
     console.log('query item: ',item);
+    //keys ve si al ingresar esta en el cache
+    const keys = client.keys(`*${item}*`);
+    console.log(keys.length);
+    
+    if (keys.length == null) {
+        console.log("No encontrado en cache");
+        // Realizar llamado grpc
+        console.log("Realizando llamado grpc");
+        (async () => {
+          await axios.get('http://grcp:3001/items', { params: {name: item}}).then(res2 => {
+          console.log("statusCode:", res2.status)
+          //console.log(res)
+          data = JSON.stringify(res2.data);
+          
+          //ingresa al cache
+          client.set(item , data)
+          console.log(await client.get(item));
+          res.json(data);
+        }).catch(error => {console.error(error)})
+        })();
+
+
+        console.log("Guardado en cache {key: value}");
+
+    }
+    else{
+        console.log("Encontrado en cache");
+        
+        console.log(keys);
+        
+        const value =  client.get(item);
+
+        console.log(value); 
+
+        res.send({ "search_results": value });
+    
+    }
+*/
+
+
+/*
       (async () => {
         await axios.get('http://grcp:3001/items', { params: {name: item}}).then(res2 => {
         console.log("statusCode:", res2.status)
@@ -38,8 +172,8 @@ app.get('/inventory/search', (req, res) => {
         res.json(data);
       }).catch(error => {console.error(error)})
       })();
-      
-    });
+ */     
+    
 //-------------
 
 app.listen(port, ()=>{
---
+++ intento/redis.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+    url: 'redis://@redis-server:6379'
+});
+
+console.log("Esperando a redis para iniciar");
+
+client.on("error", function (error) {
+    console.error(error);
+});
+  
+client.on('connect', () => {
+    console.log('Cliente Redis, conectado');
+});
+
+client.connect();
+//----------------------------------------------------------
+// Borra todo el cache de redis
+app.get("/reset", async (req, res) => {
+    await client.flushAll();
+    res.send("Cache eliminado");
+});
+
+// Obtiene todas las keys de redis
+app.get("/keys", async (req, res) => {
+    keys = await client.keys('*');
+    res.send(keys);
+});
+
+// Obtiene los valores asociados a una llave 
+// Ej.
+// http://localhost:3000/key?key=llave
+app.get("/key", async (req, res) => {
+    console.log("Searching for: " + req.query.key);
+    const key = req.query.key;
+    
+    keys = await client.keys(`*${key}*`);
+    if (keys.length == 0) {
+        console.log("No encontrado en cache");
+        res.send("No encontrado en cache");
+    }
+    else{
+        console.log("Encontrado en cache");
+        
+        console.log(keys);
+        
+        response = await client.get(key);
+
+        console.log(response); 
+
+        res.send({ value : response });
+    
+    }
+});
+
+// Ingresa un valor asociado a una llave
+// Ej.
+// http://localhost:3000/set?key=llave&value=valor
+app.get("/set", async (req, res) => {
+    let key = req.query.key;
+    let value = req.query.value;
+
+    client.set(key , value, function(err, reply) {
+        console.log(reply);
+        
+    });
+    res.send({ 
+        key: key,
+        value: value
+    });
+});
+
+// Da poderes de superprogramador 100% real no-fake
+app.get('/', function(req, res){
+    res.send("Escuchar esto mientras se evalua el trabajo: https://www.youtube.com/watch?v=2WPCLda_erI");
+});
+ 
+// Funcion principal de la tarea
+app.get("/inventory/search", async (req, res) => {
+    console.log("Buscando el termino:" + req.query.q);
+    const key = req.query.q;
+
+    keys = await client.keys(`*${key}*`);
+    
+    if (keys.length == 0) {
+        console.log("No encontrado en cache");
+        // Realizar llamado grpc
+        console.log("Realizando llamado grpc");
+            // If true
+                // Traer datos
+                
+                //// value = {set de datos}
+
+                // Guardar datos en cache
+
+                ////client.set(key , value, function(err, reply) {
+                ////    console.log(reply);   
+                ////});
+
+                console.log("Guardado en cache {key: value}");
+
+                // Mostrar datos en pantalla
+                // res.send({ "search_results": value });
+
+            
+            // else 
+                // res.send("Termino no encontrado en el inventario"); 
+
+    }
+    else{
+        console.log("Encontrado en cache");
+        
+        console.log(keys);
+        
+        value = await client.get(key);
+
+        console.log(value); 
+
+        res.send({ "search_results": value });
+    
+    }
+});
+
+app.listen(3000, '0.0.0.0', () => {
+    console.log(`SERVER RUN AT http://localhost:${3000}`);
+});
+
