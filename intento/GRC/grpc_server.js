8966923c1b3c6e02894760b6649e1a04452d91fb
wolfzart <sebastian.gonzalez8@mail.udp.cl>
2022-05-03 20:50:57 -0400

    funciona el envio

--- intento/GRC/grpc_server.js
+++ intento/GRC/grpc_server.js
@@ -4,6 +4,29 @@ const protoLoader = require("@grpc/proto-loader");
 const PROTO_PATH = "./example.proto";
 const items = require("./data.json");
 
+const { Client } = require('pg')
+const client = new Client({
+  user: 'postgres',
+  host: 'postgres',
+  database: 'tiendita',
+  password: 'marihuana',
+  port: 5432,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected! to DB");
+});
+/*
+const getTabla= async () => {
+
+  const res = await client.query('SELECT * FROM Items')
+  console.log(res.rows);
+  client.end()
+  
+}*/
+//getTabla();
+
+
 const options = {
   keepCase: true,
   longs: String,
@@ -19,9 +42,25 @@ const server = () => {
   const server = new grpc.Server();
   server.addService(itemProto.ItemService.service, {
     getItem: (_, callback) => {
+      
       const itemName = _.request.name;
+      //----------
+      console.log("DATA EXIST", itemName);
+      client.query('SELECT * FROM Items;', (err, res) => {
+        console.log(res.rows);
+        let items= null;
+        items = res.rows
+        //console.log(err ? err.stack : 'Query to Db succes');
+        const item = items.filter((obj) => obj.name.includes(itemName));
+        callback(null, { items: item});
+        //client.end()
+      })
+      //-----------
+      /*
+      console.log("item",itemName)
       const item = items.item_list.filter((obj) => obj.name.includes(itemName));
       callback(null, { items: item});
+      */
     }
   });
   server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), (err, port) => {
